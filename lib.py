553d41a8db51d10dc8bd9da26e2ca20e5a8ec08d
IcyNoctiluca <liamperston@hotmail.com>
2018-10-28 18:34:56 +0000

    finished off the init process for LOA

--- lib.py
+++ lib.py
@@ -2,22 +2,16 @@ import numpy as np
 import random
 
 
+# initialises a population of lions based on the parameters specified
+# and partitions them into the pride and nomad structures
 def generateGroups(nPop, sexRate, prideNo, percentNomad):
-
-    # init array of prideNo pride groups
-    #prideArray = np.array([Group(True) for i in range(prideNo)])
 
     # expected number of lions in each structure
     nomadPop = int(round(nPop * percentNomad, 0))
     pridePop = nPop - nomadPop
 
-    # init array of nomad lions
-    nomadLionsArray = np.array([Lion() for i in range(nomadPop)])
 
-    # init array of pride lions
-    prideLionsArray = np.array([Lion() for i in range(pridePop)])
-
-
+    ''' setting up gender distribution for lions '''
     # bit array to determine whether lion is a male
     # eg. [0,1,0,0,0,1] indicates the second and last lion to be males
     # the rest being females
@@ -25,20 +19,60 @@ def generateGroups(nPop, sexRate, prideNo, percentNomad):
     maleNomadIndicies = np.zeros(nomadPop)
 
     # the number of expected males in the population of prides and nomads
-    noPrideMales = int(round(nomadPop * (1 - sexRate), 0))
-    noNomadMales = int(round(nomadPop * (sexRate), 0))
+    noPrideMales = int(round(pridePop * (1 - sexRate), 0))
+    noNomadMales = int(round(nomadPop * sexRate, 0))
 
     # generate bit array with correct no of males
-    for i in range(noMales):
-        maleIndicies[i] = 1
+    for i in range(noPrideMales):
+        malePrideIndicies[i] = 1
+
+    for i in range(noNomadMales):
+        maleNomadIndicies[i] = 1
 
     # mix up the distribution of males a bit
-    random.shuffle(maleIndicies)
+    random.shuffle(maleNomadIndicies)
+    random.shuffle(malePrideIndicies)
 
 
-    #while
+    ''' generating lions into the structures '''
+    # init arrays of nomad and pride lions
+    nomadLionsArray = np.array([Lion() for i in range(nomadPop)])
+    prideLionsArray = np.array([Lion() for i in range(pridePop)])
 
 
+    # set attributes for nomad lions
+    for i in range(nomadPop):
+
+        nomadLionsArray[i].isNomad = True
+
+        # set gender of nomad lions
+        if maleNomadIndicies[i] == 1:
+            nomadLionsArray[i].isMale = True
+        else:
+            nomadLionsArray[i].isMale = False
+
+
+    # init array of prideNo pride groups
+    prideArray = np.array([Group(True) for i in range(prideNo)])
+
+    for i in range(pridePop):
+
+        prideLionsArray[i].isNomad = False
+
+        # set gender of pride lions
+        if malePrideIndicies[i] == 1:
+            prideLionsArray[i].isMale = True
+        else:
+            prideLionsArray[i].isMale = False
+
+
+        ''' assigning each pride lion to a pride '''
+        # index of pride to assign lion
+        # eg for 4 prides, number is 0,1,2,3
+        prideIndex = random.rand(0, prideNo - 1)
+        prideArray[prideIndex].lionArray = np.append(prideArray[prideIndex].lionArray, prideLionsArray[i])
+
+    return prideArray, nomadLionsArray
 
 
 # represents a pride or a nomad group
@@ -47,9 +81,7 @@ class Group:
     def __init__(self, isPride):
 
         self.isPride = isPride
-        self.lionArray = None
-
-
+        self.lionArray = np.array([])
 
 
 class Lion:
@@ -61,3 +93,4 @@ class Lion:
         self.groupID = None             # id of the pride or nomad of the Lion
         self.strength = None
         self.isMature = None
+        self.isNomad = None
--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+# Lion Optimisation Algorithm implementation
+# Natural computing assignment
+# Python3.6.6
+
+# library imports
 import numpy as np
 
+import lib
 
-prideNo = 4
-percentNomad = 0.2
+
+# set algorithm params as global variables
+prideNo = 4             # number of pride structures
+percentNomad = 0.2      # portion of population to be nomads
 roamingPercent = 0.2
 mutateProb = 0.2
 sexRate = 0.8
 mateProb = 0.3
 migrateRate = 0.4
-
-
 nPop = 100
